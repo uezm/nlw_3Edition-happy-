faf33aa0026efc51b2e2fd9ce249acc343375793
Daniel Uez Martins <daniel.uez@gmail.com>
2020-10-28 21:36:03 -0300

    chore: NLW 3nd Edition - Complete Project

---
+++ mobile/src/routes.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import OrphanageMap from './pages/OrphanagesMap';
+import OrphanageDetails from './pages/OrphanageDetails';
+
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+
+import Header from './components/Header';
+
+const { Navigator, Screen } = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Navigator screenOptions={{ headerShown: false, cardStyle: { backgroundColor: '#f2f3f5' } }}>
+        <Screen 
+          name="OrphanagesMap" 
+          component={OrphanageMap}
+
+        />
+
+        <Screen 
+          name="OrphanageDetails" 
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }} 
+        />
+
+        <Screen 
+          name="SelectMapPosition" 
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }} 
+        />
+
+        <Screen 
+          name="OrphanageData" 
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }} 
+        />
+
+      </Navigator>
+    </NavigationContainer>
+  );
+}
